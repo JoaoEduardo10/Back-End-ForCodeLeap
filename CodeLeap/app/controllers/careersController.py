53fe87d8a087a653971827535cb394e917aee39f
João Eduardo <joaoeduardoj920@gmail.com>
2024-02-23 16:16:45 -0300

    feat: adding careesController

--- CodeLeap/app/controllers/careersController.py
+++ CodeLeap/app/controllers/careersController.py
@@ -3,11 +3,62 @@ from app.database.serializers import CareersSerializer
 
 from rest_framework.views import APIView;
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 
 class CareersController(APIView):
-    def get(self, request):
+
+    def get_object(self, id):
+        try:
+            return Careers.objects.get(pk=id)
+        except Careers.DoesNotExist:
+            raise NotFound
+
+
+
+    def get(self, _request):
         careers = Careers.objects.all()
         serializer = CareersSerializer(careers, many=True)
 
         return Response(serializer.data)
+    
+    def post(self, request):
+
+
+        data = {
+            'title': request.data.get('title'),
+            'content': request.data.get('content'),
+            'username': request.data.get('username',)
+        }
+
+        serializer = CareersSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+    
+    def patch(self, request, id):
+        careers = self.get_object(id)
+
+        data = {
+            'title': request.data.get('title'),
+            'content': request.data.get('content'),
+            'username': request.data.get('username', careers.username),
+            'created_datetime': request.data.get('id', careers.created_datetime)
+        }
+
+        serializer = CareersSerializer(careers, data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+    
+        return Response(serializer.errors, status=404)
+    
+    def delete(self, _request, id):
+        try:
+            careers = self.get_object(id)
+            careers.delete()
+            return Response({}, status=200)
+        except Exception:
+            return Response("Unable to delete.",status=404)
--- CodeLeap/app/urls.py
+++ CodeLeap/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+
 from app.controllers.careersController import CareersController
 
 urlpatterns = [
     path('', CareersController.as_view()),
+    path('<int:id>/', CareersController.as_view()),
 ]
